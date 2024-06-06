3b0f00f1b38d6a84ab30088c9678a4e55aec23c3
cretalover <cretalover007@gmail.com>
2024-06-06 18:44:58 +0530

    Update commands.py
    support group change

--- plugins/commands.py
+++ plugins/commands.py
@@ -39,7 +39,7 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('🔺 Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ 🔺', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
         ],[
-            InlineKeyboardButton('📢 Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url="https://t.me/Razer_supportx"),
+            InlineKeyboardButton('📢 Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url="https://t.me/mahi4999"),
             InlineKeyboardButton('🔰 Mᴏᴠɪᴇ Gʀᴏᴜᴘ', url='https://t.me/moviesprimiumworld')
         ],[
             InlineKeyboardButton('🧲 Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ 🧲', url=CHNL_LNK)
